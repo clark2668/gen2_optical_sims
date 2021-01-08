44595a98c76d9d23893001ea834b4bb516c4c217
Brian Clark <baclark@msu.edu>
2021-01-08 14:04:08 -0600

    add some envs

---
+++ py2_env.sh
@@ -0,0 +1,2 @@
+eval `/cvmfs/icecube.opensciencegrid.org/py2-v3.1.1/setup.sh`
+$SROOT/metaprojects/combo/V00-00-03/env-shell.sh
---
+++ py3_env.sh
@@ -0,0 +1,2 @@
+eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.1.1/setup.sh`
+$SROOT/metaprojects/combo/V01-00-02/env-shell.sh
