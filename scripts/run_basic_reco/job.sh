5b319ec3438516b40d03c1e93c7eb38ecac947c8
Brian Clark <baclark@msu.edu>
2021-01-11 17:16:56 -0600

    add scripts to do reco

---
+++ scripts/run_basic_reco/job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# load in variables
+gcdfile=$1
+indir=$2
+outdir=$3
+design=$4
+dataset=$5
+part=$6
+
+inputfile=${indir}/${dataset}/${design}/BaseProc/${dataset}_MUONGUN_.${part}_${design}_calibrated.i3.bz2
+outfile=${dataset}_MUONGUN_.${part}_${design}_newrecos
+
+# run the reconstruction script
+ulimit -s 131072; /home/brianclark/Gen2/optical/anastasiia_gen2-scripts/Gen2_Simple_Recos.py -g ${gcdfile} -i ${inputfile} -p ${outfile}
+
+# move the outputs
+mv ${outfile}* ${outdir}/${dataset}/${design}/.
---
+++ scripts/run_basic_reco/make_dag.py
@@ -0,0 +1,24 @@
+# requires python 3! (sorry, I like f-strings...)
+
+design = 'Sunflower_240m'
+dataset = 11900
+
+source_dir = "/data/wipac/HEE/simulation/level2/no-domsim/"
+output_dir = "/data/user/brianclark/Gen2_optical/recos/"
+gcd_file = "/data/wipac/HEE/geometries/Sunflower/IceCubeHEX_Sunflower_240m_v3_ExtendedDepthRange.GCD.i3.bz2"
+
+dag_file_name = 'do_reco.dag'
+instructions = ""
+instructions += 'CONFIG config.dagman\n\n'
+
+with open(dag_file_name, 'w') as f:
+	f.write(instructions)
+
+for i in range(5):
+
+	instructions = ""
+	instructions += f'JOB job_{i} job.sub \n'
+	instructions += f'VARS job_{i} gcdfile="{gcd_file}" indir="{source_dir}" outdir="{output_dir}" design="{design}" dataset="{dataset}" part="{i:06}"\n\n'
+
+	with open(dag_file_name, 'a') as f:
+		f.write(instructions)
