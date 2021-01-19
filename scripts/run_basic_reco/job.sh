b8643beb4d673191f7b3be6315faa66310585044
Brian Clark <baclark@msu.edu>
2021-01-19 14:44:57 -0600

    update the run reco script to include capability to make mods

--- scripts/run_basic_reco/job.sh
+++ scripts/run_basic_reco/job.sh
@@ -7,12 +7,13 @@ outdir=$3
 design=$4
 dataset=$5
 part=$6
+mod=$7
 
 inputfile=${indir}/${dataset}/${design}/BaseProc/${dataset}_MUONGUN_.${part}_${design}_calibrated.i3.bz2
-outfile=${dataset}_MUONGUN_.${part}_${design}_newrecos
+outfile=${dataset}_MUONGUN_.${part}_${design}_${mod}
 
 # run the reconstruction script
-ulimit -s 131072; /home/brianclark/Gen2/optical/anastasiia_gen2-scripts/Gen2_Simple_Recos.py -g ${gcdfile} -i ${inputfile} -p ${outfile}
+ulimit -s 131072; /home/brianclark/Gen2/optical/midscale/Gen2_Simple_Recos.py -g ${gcdfile} -i ${inputfile} -p ${outfile} --geo-selection ${mod}
 
 # move the outputs
-mv ${outfile}* ${outdir}/${dataset}/${design}/.
+mv ${outfile}* ${outdir}/${dataset}/${design}/${mod}/.
--- scripts/run_basic_reco/make_dag.py
+++ scripts/run_basic_reco/make_dag.py
@@ -3,22 +3,24 @@
 design = 'Sunflower_240m'
 dataset = 11900
 
+mod='darren'
+
 source_dir = "/data/wipac/HEE/simulation/level2/no-domsim/"
 output_dir = "/data/user/brianclark/Gen2_optical/recos/"
 gcd_file = "/data/wipac/HEE/geometries/Sunflower/IceCubeHEX_Sunflower_240m_v3_ExtendedDepthRange.GCD.i3.bz2"
 
-dag_file_name = 'do_reco.dag'
+dag_file_name = 'dagman_reco.dag'
 instructions = ""
 instructions += 'CONFIG config.dagman\n\n'
 
 with open(dag_file_name, 'w') as f:
 	f.write(instructions)
 
-for i in range(5):
+for i in range(400, 1000):
 
 	instructions = ""
 	instructions += f'JOB job_{i} job.sub \n'
-	instructions += f'VARS job_{i} gcdfile="{gcd_file}" indir="{source_dir}" outdir="{output_dir}" design="{design}" dataset="{dataset}" part="{i:06}"\n\n'
+	instructions += f'VARS job_{i} gcdfile="{gcd_file}" indir="{source_dir}" outdir="{output_dir}" design="{design}" dataset="{dataset}" part="{i:06}" mod="{mod}"\n\n'
 
 	with open(dag_file_name, 'a') as f:
 		f.write(instructions)
