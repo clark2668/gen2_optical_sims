b4ac6640b0dae46e993072d7e45fd09f02726037
Brian Clark <baclark@msu.edu>
2021-02-18 08:21:11 -0600

    fix jobs so that directories are right

--- scripts/run_basic_reco/make_dag.py
+++ scripts/run_basic_reco/make_dag.py
@@ -3,20 +3,20 @@
 design = 'Sunflower_240m'
 dataset = 11900
 
-mod='darren'
+mod='corner22'
 
 source_dir = "/data/wipac/HEE/simulation/level2/no-domsim/"
-output_dir = "/data/user/brianclark/Gen2_optical/recos/"
+output_dir = "/data/user/brianclark/Gen2_optical/midscale/recos/"
 gcd_file = "/data/wipac/HEE/geometries/Sunflower/IceCubeHEX_Sunflower_240m_v3_ExtendedDepthRange.GCD.i3.bz2"
 
-dag_file_name = 'dagman_reco.dag'
+dag_file_name = 'dagman_reco_'+mod+'.dag'
 instructions = ""
 instructions += 'CONFIG config.dagman\n\n'
 
 with open(dag_file_name, 'w') as f:
 	f.write(instructions)
 
-for i in range(400, 1000):
+for i in range(1000):
 
 	instructions = ""
 	instructions += f'JOB job_{i} job.sub \n'
