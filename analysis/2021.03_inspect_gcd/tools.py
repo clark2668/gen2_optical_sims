bfd88cc9b10f21f94f9b82e63dadda9b9885706e
Brian Clark <baclark@msu.edu>
2021-03-17 19:10:37 -0500

    add  a top-down view also

--- analysis/2021.03_inspect_gcd/tools.py
+++ analysis/2021.03_inspect_gcd/tools.py
@@ -8,11 +8,23 @@ def inspect_G_frame(frame, save_plot=False):
 	om_strings = []
 	om_depths = []
 
+	om_strings_topdown = []
+	om_x = []
+	om_y = []
+
+	finished = []
+
 	for omkey, omgeo in geo:
 		if omgeo.position.z > 1000:
 			continue
 		om_strings.append(omkey.string)
 		om_depths.append(omgeo.position.z)
+
+		if omkey.om ==1:
+			if omkey.string not in finished:
+				finished.append(omkey.string)
+				om_x.append(omgeo.position.x)
+				om_y.append(omgeo.position.y)
 
 	om_strings = np.asarray(om_strings)
 	om_depths = np.asarray(om_depths)
@@ -28,7 +40,8 @@ def inspect_G_frame(frame, save_plot=False):
 	print("Max depth {}".format(np.max(om_depths)))
 
 	if save_plot:
-		# np.savez('dom_loc.npz', om_strings=om_strings, om_depths=om_depths)	
+
+		# top down
 		fig, axs = plt.subplots(1,1,figsize=(20,10))
 		axs.plot(om_strings, om_depths, 'o', markersize=3)
 		axs.set_xlabel('String Number')
@@ -37,4 +50,18 @@ def inspect_G_frame(frame, save_plot=False):
 		axs.set_ylim([-2800, -1200])
 		plt.tight_layout()
 		fig.savefig('om_map.png')
+
+		del fig, axs
+
+		fig, axs = plt.subplots(1,1,figsize=(10,10))
+		axs.plot(om_x[:86], om_y[:86], 'o', markersize=4, label='IceCube')
+		axs.plot(om_x[86:], om_y[86:], 's', markersize=4, label='Gen2')
+		axs.set_xlabel('X [m]')
+		axs.set_ylabel('Y [m]')
+		axs.set_aspect('equal')
+		axs.legend()
+		axs.grid()
+		# axs.set_ylim([-2800, -1200])
+		plt.tight_layout()
+		fig.savefig('top_down.png')
 
